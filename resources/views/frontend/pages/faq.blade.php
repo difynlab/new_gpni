4ba587ef8a10ebb93dab88911d3d0ae28cbbcb1b
chagana <chaga@sk.epirco.com>
2024-11-23 14:37:01 +0530

    faq page

--- resources/views/frontend/pages/faq.blade.php
+++ resources/views/frontend/pages/faq.blade.php
@@ -10,21 +10,21 @@
 
     <div class="container my-5">
         @if($contents->title_en)
-            <div class="faq-title pt-5">{{ $contents->{'title_' . $middleware_language} ?? $contents->title_en }}</div>
+            <div class="faq-title fs-61 pt-md-5 pt-0">{{ $contents->{'title_' . $middleware_language} ?? $contents->title_en }}</div>
 
             <div class="accordion pt-5" id="faqAccordion">
                 @if($faqs->isNotEmpty())
                     @foreach($faqs as $faq)
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="heading{{ $faq->id }}">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                <button class="accordion-button d-flex justify-content-center align-items-center collapsed fs-25" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#collapse{{ $faq->id }}" aria-expanded="false" aria-controls="collapse{{ $faq->id }}">
                                     {{ $faq->question }}
                                 </button>
                             </h2>
                             <div id="collapse{{ $faq->id }}" class="accordion-collapse collapse" aria-labelledby="heading{{ $faq->id }}"
                                 data-bs-parent="#faqAccordion">
-                                <div class="accordion-body">
+                                <div class="accordion-body fs-25">
                                     {!! $faq->answer !!}
                                 </div>
                             </div>
