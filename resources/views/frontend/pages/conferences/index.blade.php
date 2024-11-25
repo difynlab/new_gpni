f9d1b0a92cfce6e7632f1c07887cea2771aa6e66
chagana <chaga@sk.epirco.com>
2024-11-26 00:31:31 +0530

    conference main and single page responsiveness

--- resources/views/frontend/pages/conferences/index.blade.php
+++ resources/views/frontend/pages/conferences/index.blade.php
@@ -3,38 +3,40 @@
 @section('title', 'Conference')
 
 @push('after-styles')
-    <link rel="stylesheet" href="{{ asset('frontend/css/conferences.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/conferences.css') }}">
 @endpush
 
 @section('content')
 
-    @if($contents->title_en)
-        <div class="container mt-5 pt-5">
-            <h1 class="heading pt-5">{{ $contents->{'title_' . $middleware_language} ?? $contents->title_en }}</h1>
-            <div class="card-container">
-                @if($conferences->isNotEmpty())
-                    @foreach($conferences as $conference)
-                        <div class="conference-card">
-                            <div class="text-section">
-                                <div class="title"> {{ $conference->title }}</div>
-                                <div class="item">
-                                    <div class="label">Date:</div>
-                                    <div class="value">{{ $conference->date }}</div>
-                                </div>
-                                <div class="item">
-                                    <div class="label">Venue:</div>
-                                    <div class="value">{{ $conference->where }}</div>
-                                </div>
-                            </div>
-                            <a href="{{ route('frontend.conferences.show', $conference) }}" class="view-more">
-                                <span>View More details</span>
-                                <img src="{{ asset('storage/frontend/small-arrow-right.svg') }}" alt="Arrow Icon">
-                            </a>
-                        </div>
-                    @endforeach
-                @endif
+@if($contents->title_en)
+<div class="container mt-5 pt-lg-5 pt-0">
+    <h1 class="heading py-lg-5 my-5">{{ $contents->{'title_' . $middleware_language} ?? $contents->title_en }}</h1>
+    <div class="row card-container">
+        @if($conferences->isNotEmpty())
+        @foreach($conferences as $conference)
+        <div class="col-12 col-md-6 mb-4">
+            <div class="conference-card">
+                <div class="text-section">
+                    <div class="title fs-20 line-clamp-2">{{ $conference->title }}</div>
+                    <div class="item">
+                        <div class="label fs-16">Date:</div>
+                        <div class="value fs-16">{{ $conference->date }}</div>
+                    </div>
+                    <div class="item">
+                        <div class="label fs-16">Venue:</div>
+                        <div class="value fs-16 text-truncate">{{ $conference->where }}</div>
+                    </div>
+                </div>
+                <a href="{{ route('frontend.conferences.show', $conference) }}" class="view-more fs-16">
+                    <span>View More details</span>
+                    <img src="{{ asset('storage/frontend/small-arrow-right.svg') }}" alt="Arrow Icon">
+                </a>
             </div>
         </div>
-    @endif
+        @endforeach
+        @endif
+    </div>
+</div>
+@endif
 
 @endsection
