150205e9e8323057ec7ef7ae32c0dc4a6038424b
Chagana05 <chaga@sk.epirco.com>
2024-12-03 09:27:37 +0530

    register, login,captcha

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -11,7 +11,7 @@
 
     <div class="container my-5">
         <div class="row flex-grow-1 d-flex align-items-center">
-            
+
             <x-frontend.auth></x-frontend.auth>
 
             <div class="col-lg-8 white-section" style="min-height: 500px;">
@@ -23,58 +23,81 @@
                 <form method="POST" action="{{ route('frontend.register.store') }}">
                     @csrf
 
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="firstName" class="form-label">First Name</label>
-                            <input type="text" class="form-control" id="firstName" name="first_name" placeholder="Enter your first name" value="{{ old('first_name') }}" required>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="firstName" class="form-label">First Name</label>
+                                <input type="text" class="form-control" id="firstName" name="first_name"
+                                    placeholder="Enter your first name" value="{{ old('first_name') }}" required>
+                            </div>
                         </div>
-                        <div class="form-group col-md-6">
-                            <label for="lastName" class="form-label">Last Name</label>
-                            <input type="text" class="form-control" id="lastName" name="last_name" placeholder="Enter your last name" value="{{ old('last_name') }}" required>
-                        </div>
-                    </div>
-
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="country" class="form-label">Country</label>
-                            <select class="form-control country-select" id="country" name="country" required>
-                                <option value="">Select Country</option>
-                                @foreach($countries as $country)
-                                    <option value="{{ $country }}" {{ old('country') == $country ? 'selected' : '' }}>{{ $country }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label for="inputEmail4" class="form-label">Email Address</label>
-                            <input type="email" class="form-control" id="inputEmail4" name="email" placeholder="Ex: johnmercury@gmail.com" value="{{ old('email') }}" required>
-                            <x-frontend.input-error field="email"></x-frontend.input-error>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="lastName" class="form-label">Last Name</label>
+                                <input type="text" class="form-control" id="lastName" name="last_name"
+                                    placeholder="Enter your last name" value="{{ old('last_name') }}" required>
+                            </div>
                         </div>
                     </div>
 
-                    <div class="form-row">
-                        <div class="form-group col-md-6 position-relative">
-                            <label for="inputPassword4" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="inputPassword4" name="password" placeholder="********" required>
-                            <span class="bi bi-eye-slash-fill toggle-password"></span>
-                            <x-frontend.input-error field="password"></x-frontend.input-error>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="country" class="form-label">Country</label>
+                                <select class="form-control country-select" id="country" name="country" required>
+                                    <option value="">Select Country</option>
+                                    @foreach ($countries as $country)
+                                        <option value="{{ $country }}"
+                                            {{ old('country') == $country ? 'selected' : '' }}>{{ $country }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                         </div>
-                        <div class="form-group col-md-6 position-relative">
-                            <label for="inputConfirmPassword4" class="form-label">Confirm Password</label>
-                            <input type="password" class="form-control" id="inputConfirmPassword4" name="confirm_password" placeholder="********" required>
-                            <span class="bi bi-eye-slash-fill toggle-password"></span>
-                            <x-frontend.input-error field="confirm_password"></x-frontend.input-error>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="inputEmail4" class="form-label">Email Address</label>
+                                <input type="email" class="form-control" id="inputEmail4" name="email"
+                                    placeholder="Ex: johnmercury@gmail.com" value="{{ old('email') }}" required>
+                                <x-frontend.input-error field="email"></x-frontend.input-error>
+                            </div>
                         </div>
                     </div>
 
-                    <div class="form-row">
-                        <div class="form-group col-md-12">
-                            <label for="language" class="form-label">Select Language</label>
-                            <select class="form-select country-select" id="language" name="language" required>
-                                <option value="">Select your language</option>
-                                <option value="English" {{ old('language') == 'English' ? 'selected' : '' }}>English</option>
-                                <option value="Japanese" {{ old('language') == 'Japanese' ? 'selected' : '' }}>Japanese</option>
-                                <option value="Chinese" {{ old('language') == 'Chinese' ? 'selected' : '' }}>Chinese</option>
-                            </select>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group position-relative">
+                                <label for="inputPassword4" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="inputPassword4" name="password"
+                                    placeholder="********" required>
+                                <span class="bi bi-eye-slash-fill toggle-password"></span>
+                                <x-frontend.input-error field="password"></x-frontend.input-error>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group position-relative">
+                                <label for="inputConfirmPassword4" class="form-label">Confirm Password</label>
+                                <input type="password" class="form-control" id="inputConfirmPassword4"
+                                    name="confirm_password" placeholder="********" required>
+                                <span class="bi bi-eye-slash-fill toggle-password"></span>
+                                <x-frontend.input-error field="confirm_password"></x-frontend.input-error>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="language" class="form-label">Select Language</label>
+                                <select class="form-select country-select" id="language" name="language" required>
+                                    <option value="">Select your language</option>
+                                    <option value="English" {{ old('language') == 'English' ? 'selected' : '' }}>English
+                                    </option>
+                                    <option value="Japanese" {{ old('language') == 'Japanese' ? 'selected' : '' }}>Japanese
+                                    </option>
+                                    <option value="Chinese" {{ old('language') == 'Chinese' ? 'selected' : '' }}>Chinese
+                                    </option>
+                                </select>
+                            </div>
                         </div>
                     </div>
 
@@ -83,7 +106,9 @@
                     </div>
 
                     <div class="d-flex justify-content-end">
-                        <button type="submit" class="btn btn-primary btn-block submit-button" style="background-color: #0040c3; color: #fff; border: none; border-radius: 10px; height: 46px;" disabled>Submit</button>
+                        <button type="submit" class="btn btn-primary btn-block submit-button"
+                            style="background-color: #0040c3; color: #fff; border: none; border-radius: 10px; height: 46px;"
+                            disabled>Submit</button>
                     </div>
                 </form>
             </div>
@@ -95,13 +120,12 @@
 @push('after-scripts')
     <script src="{{ asset('frontend/js/captcha.js') }}"></script>
     <script>
-        $(".toggle-password").click(function () {
+        $(".toggle-password").click(function() {
             $(this).toggleClass("bi-eye-slash-fill bi-eye-fill");
-            
-            if($(this).prev().attr("type") == "password") {
+
+            if ($(this).prev().attr("type") == "password") {
                 $(this).prev().attr("type", "text");
-            }
-            else {
+            } else {
                 $(this).prev().attr("type", "password");
             }
         });
