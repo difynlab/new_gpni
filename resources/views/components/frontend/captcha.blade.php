c1f5ff95a3a1bc0a0e507a2b02193180c820fe42
imzajjithvedha <zajjith@gmail.com>
2024-12-03 10:48:52 +0530

    captcha issue

--- resources/views/components/frontend/captcha.blade.php
+++ resources/views/components/frontend/captcha.blade.php
@@ -1,15 +1,11 @@
-<div class="row captcha">
-    <div class="col-12 col-md-4 mb-3 mb-md-0">
-        <p class="text text-center">Validate Captcha</p>
+<div class="captcha d-flex align-items-center justify-content-between p-3 rounded">
+    <span class="text fs-16">Validate Captcha</span>
+    <div class="captcha-equation py-md-0 py-2 d-flex gap-2">
+        <span class="number number-1 fs-16"></span>
+        <span>+</span>
+        <span class="number number-2 fs-16"></span>
+        <span>=</span>
+        <input type="text" class="form-control sum-value fs-16" placeholder="?" required>
     </div>
-    <div class="col-12 col-md-8">
-        <div class="d-flex flex-wrap justify-content-center align-items-center gap-2">
-            <p class="number number-1 mb-0"></p>
-            <p class="number mb-0">+</p>
-            <p class="number number-2 mb-0"></p>
-            <p class="number mb-0">=</p>
-            <input type="text" class="form-control sum-value mb-0" placeholder="?" required>
-            <button type="button" class="btn verify-button">Verify</button>
-        </div>
-    </div>
+    <button type="button" class="btn verify-button fs-16">Verify</button>
 </div>
